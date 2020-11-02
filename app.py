f4a950b7bc71d21500c7b15be024e50691384343
OmarIbrahim991 <omar.ibrahim.991@gmail.com>
2020-11-02 05:45:49 +0200

    add eventlet monkey patch

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import eventlet
+eventlet.monkey_patch()
 from flask import Flask, render_template, redirect, url_for, make_response, send_from_directory, request, session, flash
 from flask_socketio import SocketIO
 from flask_cors import CORS
